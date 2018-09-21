a5d37c4301d4c1223288ce3187e7da5e9335512f
Palitop <35600848+Palitop@users.noreply.github.com>
2018-09-21 19:34:01 +0200

    Create Ventana.java

---
+++ Ventana.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+
+    public Ventana() {
+        this.setSize(200, 200);
+        this.setTitle("ventana");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        initComponents();
+    }
+
+    private void initComponents() {
+        
+    }
+}
